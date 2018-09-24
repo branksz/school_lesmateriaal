99f39bba39cb1132dedf726b33d8b9be2a859796
Branko <branko@pixeldeluxe.nl>
2018-09-24 22:27:44 +0200

    Grote en kleine jumbotron includes gemaakt

---
+++ resources/views/inc/jumbotron-small.blade.php
@@ -0,0 +1,7 @@
+<div class="jumbotron jumbotron--hero p-5 mb-0">
+	<div class="container">
+		<div class="col-12 offset-md-1 col-md-10">
+			<h1 class="text-white"><strong>{{ $title }}</strong></h1>
+		</div>
+	</div>
+</div>
--- resources/views/inc/jumbotron.blade.php
+++ resources/views/inc/jumbotron.blade.php
@@ -1,8 +1,8 @@
-<div class="jumbotron">
-	<div class="container">
+<div class="jumbotron jumbotron--hero">
+	<div class="container pt-5 pb-5">
 		<div class="col-12">
-			<h1>Les Materiaal</h1>
-			<h2>De leukste manier om te leren</h2>
+			<h1 class="text-white">Les Materiaal</h1>
+			<h2 class="text-white">De leukste manier om te leren</h2>
 
 			<button class="btn btn-primary p-2 pl-3 pr-3 mt-3">Lees meer</button>
 		</div>
