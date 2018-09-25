56759f81c438e9dffa33673e23188ad715708399
Branko <branko@pixeldeluxe.nl>
2018-09-25 19:31:46 +0200

    Role toevoegen aan default user registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,14 +63,11 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $user = User::create([
+        return User::create([
             'name'     => $data['name'],
             'email'    => $data['email'],
+            'role'     => 'school',
             'password' => Hash::make($data['password']),
         ]);
-
-        $user->roles()->attach(Role::where('name', 'school')->first());
-
-        return $user;
     }
 }
