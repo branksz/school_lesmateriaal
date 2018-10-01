1f2d94a770b01ba55c21e719411a31a503021e73
Branko <branko@pixeldeluxe.nl>
2018-10-01 16:31:55 +0200

    Responsive meta tag instellen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,8 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>Lesmateriaal</title>
 	<link rel="stylesheet" href="/css/app.css" />
 </head>
