a6bfdd7bbd32baf304f2206bdc5aa10199b8db8b
Branko <branko@pixeldeluxe.nl>
2018-10-01 15:04:23 +0200

    Indelen dashboard items WIP

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,16 @@
 	<div class="row">
 		<div class="col-12 offset-md-1 col-md-10">
 			<div class="row row--white pb-3">
-				
+				<div class="col-12 col-lg-8 mt-4">
+					<p class="h2">De prins trouwt, de wereld glimlacht</p>
+				</div>
+				<div class="col-12 col-lg-4 mt-4">
+					<p class="h4">Recent lesmateriaal</p>
+					<a href="" class="btn btn-info d-block text-white m-3 pb-3 pt-3">Week 5</a>
+					<a href="" class="btn btn-info d-block text-white m-3 pb-3 pt-3">Week 5</a>
+					<a href="" class="btn btn-info d-block text-white m-3 pb-3 pt-3">Week 5</a>
+					<a href="" class="btn btn-secondary d-block text-white m-3 pb-3 pt-3">Bekijk alles <i class="fa fa-arrow-right"></i></a>
+				</div>
 			</div>
 		</div>
 	</div>
--- resources/views/inc/jumbotron-small.blade.php
+++ resources/views/inc/jumbotron-small.blade.php
@@ -1,10 +1,12 @@
 <div class="jumbotron jumbotron--hero p-5 mb-0">
 	<div class="container">
-		<div class="col-12 offset-md-1 col-md-10">
-			<h1 class="text-white d-inline-block"><strong>{{ $title }}</strong></h1>
-			@if(!empty($button) && $button == true)
-				<a href="" class="btn btn-primary float-right">Stuur een onderwerp in</a>
-			@endif
+		<div class="row">
+			<div class="col-12 offset-md-1 col-md-10">
+				<h1 class="text-white d-inline-block"><strong>{{ $title }}</strong></h1>
+				@if(!empty($button) && $button == true)
+					<a href="" class="btn btn-primary float-right">Stuur een onderwerp in</a>
+				@endif
+			</div>			
 		</div>
 	</div>
 </div>
