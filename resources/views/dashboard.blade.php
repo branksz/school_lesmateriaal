18659056464b62f1a65810ce55efc8a99611bc7d
Branko <branko@pixeldeluxe.nl>
2018-10-01 16:32:23 +0200

    3 recente items uitlezen op dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,19 +3,26 @@
 @section('content')
 
 	@include('inc.jumbotron-small', ['title' => 'Dashboard', 'button' => true])
+	@php $entry = $entries[0] @endphp
 
 	<div class="row">
 		<div class="col-12 offset-md-1 col-md-10">
 			<div class="row row--white pb-3">
 				<div class="col-12 col-lg-8 mt-4">
-					<p class="h2">De prins trouwt, de wereld glimlacht</p>
+					<div class="newestItem">
+						<p class="h2">{{ $entry['name'] }}</p>
+						<img src="{{ url('/img/' . $entry['imagename']) }}" class="img-fluid" alt="{{ $entry['name'] }}" />
+						<p class="mt-2"><i>{{ str_limit($entry['introduction'], 200) }}</i></p>
+						<a href="">Lees verder <i class="fa fa-arrow-right"></i></a>
+					</div>
 				</div>
 				<div class="col-12 col-lg-4 mt-4">
 					<p class="h4">Recent lesmateriaal</p>
-					<a href="" class="btn btn-info d-block text-white m-3 pb-3 pt-3">Week 5</a>
-					<a href="" class="btn btn-info d-block text-white m-3 pb-3 pt-3">Week 5</a>
-					<a href="" class="btn btn-info d-block text-white m-3 pb-3 pt-3">Week 5</a>
-					<a href="" class="btn btn-secondary d-block text-white m-3 pb-3 pt-3">Bekijk alles <i class="fa fa-arrow-right"></i></a>
+					{{-- loopen door het lesmateriaal --}}
+					@foreach ($entries as $entry)
+						<a href="" class="btn btn-info d-block text-white ml-0 mt-2 mb-2 pb-3 pt-3">Week {{ $entry['week'] }}</a>
+					@endforeach
+					<a href="" class="btn btn-secondary d-block text-white ml-0 mt-2 mb-2 pb-1 pt-1">Bekijk alles <i class="fa fa-arrow-right"></i></a>
 				</div>
 			</div>
 		</div>
