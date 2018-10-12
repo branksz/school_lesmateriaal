9d5943531ea0577b1886d31dd0a8ce4ff597f1c8
Branko <branko@pixeldeluxe.nl>
2018-10-12 15:51:07 +0200

    Dashboard index uitbreiden met link naar overzicht

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -14,16 +14,16 @@
 						<p class="h2">{{ $entry['name'] }}</p>
 						<img src="{{ url('/img/' . $entry['imagename']) }}" class="img-fluid" alt="{{ $entry['name'] }}" />
 						<p class="mt-2"><i>{{ str_limit($entry['introduction'], 200) }}</i></p>
-						<a href="{{ $entry['slug'] }}">Lees verder <i class="fa fa-arrow-right"></i></a>
+						<a href="{{ url('/dashboard/materiaal/' . $entry['slug']) }}">Lees verder <i class="fa fa-arrow-right"></i></a>
 					</div>
 				</div>
 				<div class="col-12 col-lg-4 mt-4">
 					<p class="h4">Recent lesmateriaal</p>
 					{{-- loopen door het lesmateriaal --}}
 					@foreach ($entries as $entry)
-						<a href="{{ $entry['slug'] }}" class="btn btn-info d-block text-white ml-0 mt-2 mb-2 pb-3 pt-3">Week {{ $entry['week'] }}</a>
+						<a href="{{ url('/dashboard/materiaal/' . $entry['slug']) }}" class="btn btn-info d-block text-white ml-0 mt-2 mb-2 pb-3 pt-3">Week {{ $entry['week'] }}</a>
 					@endforeach
-					<a href="" class="btn btn-secondary d-block text-white ml-0 mt-2 mb-2 pb-1 pt-1">Bekijk alles <i class="fa fa-arrow-right"></i></a>
+					<a href="{{ url('/dashboard/materiaal') }}" class="btn btn-secondary d-block text-white ml-0 mt-2 mb-2 pb-1 pt-1">Bekijk alles <i class="fa fa-arrow-right"></i></a>
 				</div>
 			</div>
 		</div>
