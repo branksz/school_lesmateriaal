1b49ec4e73993ab6ec8d14f7689b98d38a7f0bf2
Branko <branko@pixeldeluxe.nl>
2018-10-12 15:51:34 +0200

    Entry pagina lesmateriaal button insturen weggehaald

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,10 +19,22 @@ class DashboardController extends Controller
     	return view('dashboard/index', ['entries' => $entry]);
     }
 
+    public function allMaterial()
+    {
+        $entries = \DB::table('lessonmaterial')->orderBy('id', 'desc')->limit(null)->get();
+        return view('dashboard/allmaterial', ['entries' => $entries]);
+    }
+
     // lesmateriaal view returnen met lesmateriaal
     public function getMaterial($slug = null)
     {
         $entry = \DB::table('lessonmaterial')->where('slug', $slug)->get();
-        return view('dashboard/_entry', ['entry' => $entry]);
+
+        // indien niet gevonden
+        if (sizeof($entry) > 0) {
+            return view('dashboard/_entry', ['entry' => $entry[0]]);
+        }
+
+        return view('404');
     }
 }
--- resources/views/dashboard/_entry.blade.php
+++ resources/views/dashboard/_entry.blade.php
@@ -2,28 +2,20 @@
 
 @section('content')
 
-	@include('inc.jumbotron-small', ['title' => 'Lesmateriaal', 'button' => true])
+	@include('inc.jumbotron-small', ['title' => $entry['name'], 'button' => false])
 
 	<div class="row">
 		<div class="col-12 offset-md-1 col-md-10">
 			<div class="row row--white pb-3">
 				<div class="col-12 col-lg-8 mt-4">
-					
-					{{-- <p class="h2">Direct aan de slag</p>
-					<div class="newestItem mt-3 p-3 border-grey">
-						<p class="h2">{{ $entry['name'] }}</p>
-						<img src="{{ url('/img/' . $entry['imagename']) }}" class="img-fluid" alt="{{ $entry['name'] }}" />
-						<p class="mt-2"><i>{{ str_limit($entry['introduction'], 200) }}</i></p>
-						<a href="">Lees verder <i class="fa fa-arrow-right"></i></a>
-					</div> --}}
+					<img src="{{ url('/img/' . $entry['imagename']) }}" class="img-fluid" alt="{{ $entry['name'] }}" />
+					<p class="mt-2"><i>{{ $entry['introduction'] }}</i></p>
 				</div>
 				<div class="col-12 col-lg-4 mt-4">
-					<p class="h4">Recent lesmateriaal</p>
-					{{-- loopen door het lesmateriaal --}}
-					{{-- @foreach ($entries as $entry)
-						<a href="" class="btn btn-info d-block text-white ml-0 mt-2 mb-2 pb-3 pt-3">Week {{ $entry['week'] }}</a>
-					@endforeach --}}
-					<a href="" class="btn btn-secondary d-block text-white ml-0 mt-2 mb-2 pb-1 pt-1">Bekijk alles <i class="fa fa-arrow-right"></i></a>
+					<p class="text-center bg-secondary d-block text-white ml-0 mt-2 mb-2 pb-3 pt-3">Week {{ $entry['week'] }}</p>
+					<a download href="{{ url('/files/' . $entry['filename']) }}" class="btn btn-primary d-block text-white ml-0 mt-2 mb-2 pb-3 pt-3">Download PDF <i class="fa fa-download"></i></a>
+
+					<a href="{{ url('/dashboard/materiaal') }}" class="btn btn-info d-block text-white ml-0 mt-2 mb-2 pb-1 pt-1">Bekijk overzicht <i class="fa fa-arrow-right"></i></a>
 				</div>
 			</div>
 		</div>
