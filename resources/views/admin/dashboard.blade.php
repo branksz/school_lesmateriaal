5681e498b4e57abeafb3c9070c719197f14d3f46
Branko <branko@pixeldeluxe.nl>
2018-11-04 14:25:31 +0100

    link naar inzendingen uit dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,7 +8,7 @@
 		<div class="col-12 offset-md-1 col-md-10">
 			<div class="row row--white pb-3">
 				<div class="col-12 offset-md-1 col-md-6">
-					<a href="" class="btn btn-info d-block text-white ml-0 mt-2 mb-2 pb-3 pt-3">Gebruikers overzicht</a>
+					<a href="{{ url('/admin/inzendingen') }}" class="btn btn-info d-block text-white ml-0 mt-2 mb-2 pb-3 pt-3">Inzendingen overzicht</a>
 					<a href="{{ url('/admin/lesmateriaal') }}" class="btn btn-info d-block text-white ml-0 mt-2 mb-2 pb-3 pt-3">Materiaal overzicht</a>
 				</div>
 			</div>
