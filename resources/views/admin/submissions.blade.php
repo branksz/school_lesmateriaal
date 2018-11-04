08b2949f1af8920fb86ec784a5dedbc9304c3ee3
Branko <branko@pixeldeluxe.nl>
2018-11-04 15:24:52 +0100

    Contact formulier overzicht

--- resources/views/admin/submissions.blade.php
+++ resources/views/admin/submissions.blade.php
@@ -8,11 +8,32 @@
 		<div class="col-12 offset-md-1 col-md-10">
 			<div class="row row--white pb-3">
 				<div class="col-12 mt-1">
-					<ul class="mt-3">
-						@foreach ($entries as $entry)
-							<li>{{ $entry['subject'] }}</li>
-						@endforeach
-					</ul>
+					@if (empty('contact'))
+						<ul class="mt-3">
+							@foreach ($entries as $entry)
+								<li>{{ $entry['subject'] }}</li>
+							@endforeach
+						</ul>
+					@else
+						<table class="mt-3">
+							<thead>
+								<tr>
+									<th>Naam</th>
+									<th>Email</th>
+									<th>Bericht</th>
+								</tr>
+							</thead>
+							<tbody>
+								@foreach ($entries as $entry)
+									<tr>
+										<td>{{ $entry['name'] }}</td>
+										<td>{{ $entry['email'] }}</td>
+										<td>{{ $entry['message'] }}</td>
+									</tr>
+								@endforeach
+							</tbody>
+						</table>
+					@endif
 				</div>
 			</div>
 		</div>
